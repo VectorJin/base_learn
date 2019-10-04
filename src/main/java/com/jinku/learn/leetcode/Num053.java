bc484bfb981388b1909f686b196b3a6abefbccc2
lijk <lijk@jiedaibao.com>
2019-10-04 16:01:27 +0800

    最大子序和

---
+++ src/main/java/com/jinku/learn/leetcode/Num053.java
@@ -0,0 +1,47 @@
+package com.jinku.learn.leetcode;
+
+/**
+ * 最大子序和
+ */
+public class Num053 {
+
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.length) {
+            if (nums[left] > max) {
+                max = nums[left];
+            }
+            if (nums[left] <= 0) {
+                sum = 0;
+                left++;
+                right++;
+                continue;
+            }
+
+            if (left == right) {
+                sum = nums[left];
+                right++;
+            } else {
+                if (nums[right] + sum <= 0) {
+                    left = right + 1;
+                    right++;
+                    sum = 0;
+                } else {
+                    sum += nums[right];
+                    if (sum > max) {
+                        max = sum;
+                    }
+                    right++;
+                }
+            }
+
+        }
+
+        return max;
+    }
+}
