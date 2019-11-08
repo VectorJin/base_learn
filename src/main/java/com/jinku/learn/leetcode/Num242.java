375e19ae66999136d6f0396e52b35641b5b55bc7
lijk <lijk@jiedaibao.com>
2019-11-08 16:46:56 +0800

    有效的字母异位词

--- src/main/java/com/jinku/learn/leetcode/Num242.java
+++ src/main/java/com/jinku/learn/leetcode/Num242.java
@@ -9,30 +9,19 @@ import java.util.Map;
 public class Num242 {
 
     public boolean isAnagram(String s, String t) {
-
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            int count = sMap.getOrDefault(c, 0);
-            sMap.put(c, count);
-        }
-
-        for (char c : t.toCharArray()) {
-            int count = tMap.getOrDefault(c, 0);
-            tMap.put(c, count);
-        }
-
-        if (sMap.size() != tMap.size()) {
+        if (s.length() != t.length()) {
             return false;
         }
-
-        for (Map.Entry<Character, Integer> entry : sMap.entrySet()) {
-            Integer count = tMap.get(entry.getKey());
-            if (count == null || count.equals(entry.getValue())) {
-                return  false;
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
