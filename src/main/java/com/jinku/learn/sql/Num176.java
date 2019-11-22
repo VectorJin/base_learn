b3bb72a8b302b418ac3283bb21d65ec95580421e
lijk <lijk@jiedaibao.com>
2019-11-22 19:38:00 +0800

    第二高的薪水

---
+++ src/main/java/com/jinku/learn/sql/Num176.java
@@ -0,0 +1,12 @@
+package com.jinku.learn.sql;
+
+/**
+ * 第二高的薪水
+ */
+public class Num176 {
+
+    /**
+     * select (select distinct(Salary) from Employee
+     * order by Salary desc limit 1,1) as SecondHighestSalary
+     */
+}
