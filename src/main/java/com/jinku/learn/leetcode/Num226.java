5257cda373725f6312f89f5a0abc7b157dbf1f0a
lijk <lijk@jiedaibao.com>
2019-12-10 21:19:28 +0800

    翻转二叉树

---
+++ src/main/java/com/jinku/learn/leetcode/Num226.java
@@ -0,0 +1,29 @@
+package com.jinku.learn.leetcode;
+
+/**
+ * 翻转二叉树
+ */
+public class Num226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
