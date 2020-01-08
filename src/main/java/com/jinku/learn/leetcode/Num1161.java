cebea7e4a9eb84f7c502a46d6121cd5d3093a339
lijk <lijk@jiedaibao.com>
2020-01-08 18:19:22 +0800

    最大层内元素和 2

--- src/main/java/com/jinku/learn/leetcode/Num1161.java
+++ src/main/java/com/jinku/learn/leetcode/Num1161.java
@@ -1,6 +1,8 @@
 package com.jinku.learn.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -9,6 +11,42 @@ import java.util.Queue;
 public class Num1161 {
 
     public int maxLevelSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int maxLevel = 0;
+        int level = 1;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int sum = 0;
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            if (sum > max) {
+                max = sum;
+                maxLevel = level;
+            }
+            level++;
+        }
+        return maxLevel;
+    }
+
+    public int maxLevelSum2(TreeNode root) {
         if (root == null) {
             return 0;
         }
